608089176d54d743c99ef3cca2c8e197432966f9
milan.jayawardane@inovaitsys.com <milanjayawardane@Milans-MacBook-Air.local>
2019-05-28 22:03:17 +0530

    Initial source commit

---
+++ RWPickFlavor/RWPickFlavor.swift
@@ -0,0 +1,30 @@
+//
+//  RWPickFlavor.swift
+//  RWPickFlavor
+//
+//  Created by Milan Jayawardane on 5/28/19.
+//  Copyright © 2019 Milan Jayawardane. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+public protocol RWPickFlavorProtocol {
+    func didCallHello()
+}
+
+class RWPickFlavor: NSObject {
+
+    public static let shared = RWPickFlavor()
+    
+    public var delegate: RWPickFlavorProtocol?
+    
+    open func hello(){
+        debugPrint("Hello from RWPickFlavor!")
+        RWPickFlavor.shared.delegate?.didCallHello()
+        
+        Alamofire.request("https://apple.com").downloadProgress { (progress) in
+            debugPrint(progress)
+        }
+    }
+}
